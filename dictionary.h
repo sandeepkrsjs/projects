fc4044cd884734cab68d083437fb4bf2a1a6b374
sandeepkrsjs <sandeepkrsjs@users.noreply.github.com>
2017-05-17 08:59:55 +0000

    20170517T085853Z

---
+++ dictionary.h
@@ -0,0 +1,43 @@
+/**
+ * Declares a dictionary's functionality.
+ */
+
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+
+#include <stdbool.h>
+
+typedef struct node
+{
+    bool is_word;
+    struct node *children[27];
+}
+node;
+
+void free_nodes(node* ptr);
+
+// maximum length for a word
+// (e.g., pneumonoultramicroscopicsilicovolcanoconiosis)
+#define LENGTH 45
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char *word);
+
+/**
+ * Loads dictionary into memory. Returns true if successful else false.
+ */
+bool load(const char *dictionary);
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void);
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void);
+
+#endif // DICTIONARY_H
